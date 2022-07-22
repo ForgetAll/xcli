11c7626f31ea18d237cfa24bca6b791a6fff0deb
ForgetAll <xiasuhuei321@163.com>
2022-07-22 14:21:54 +0800

    feat: add version command

--- cmd/version.go
+++ cmd/version.go
@@ -1,3 +1,24 @@
 package cmd
 
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
 const version = "v0.0.2"
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of xcli",
+	Long:  `All software has versions. This is xcli's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("xcli", version)
+		cmd.Println()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
