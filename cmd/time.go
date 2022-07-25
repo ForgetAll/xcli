3104857ee89f1493812047d79d67144b9fdb8b55
ForgetAll <xiasuhuei321@163.com>
2022-07-25 14:17:35 +0800

    feat: add param for time cmd

--- cmd/time.go
+++ cmd/time.go
@@ -6,19 +6,44 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	showType int
+
+	showNormal    = 0
+	showUnix      = 1
+	showUnixMilli = 2
+
+	showSeconds bool
+	showMilli   bool
+)
+
 // timeCmd represents the time command
 var timeCmd = &cobra.Command{
 	Use:   "time",
 	Short: "get current time",
 	Long:  `get current time`,
 	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Println(time.Now().Format(time.RFC3339))
-		// cmd.Println(time.Now().Format("1"))
-		// cmd.Println(time.Now().Format("2006"))
-		// cmd.Println(time.Now().Format("2"))
+		if showSeconds {
+			showType = showUnix
+		}
+		if showMilli {
+			showType = showUnixMilli
+		}
+
+		now := time.Now()
+		switch showType {
+		case showNormal:
+			cmd.Println(now.Format(time.RFC3339))
+		case showUnix:
+			cmd.Println(now.Unix())
+		case showUnixMilli:
+			cmd.Println(now.UnixMilli())
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(timeCmd)
+	timeCmd.PersistentFlags().BoolVarP(&showSeconds, "ts", "", false, "show time stamp in seconds")
+	timeCmd.PersistentFlags().BoolVarP(&showMilli, "tms", "", false, "show time stamp in milliseconds")
 }
